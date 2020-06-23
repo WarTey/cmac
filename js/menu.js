1133ff5a72164eb2409063b05c0c9bc53d705c32
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-06-24 01:08:03 +0200

    Suppresion d'un écart 'width'

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,9 @@
 $(document).ready(function() {
 	var rotation = 0;
-	$("#arrow").click(function() {
+	$(".arrow").click(function() {
 		rotation += 60;
 		$(".menu-listings").css({
 			"transform" : 'rotate(' + rotation + 'deg)'
-		});
-		$(".menu-toggle").css({
-			"transform" : 'rotate(' + (-1 * rotation) + 'deg)'
 		});
 	});
 
