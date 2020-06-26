dfe5eae1ea6ef8e3d7d7a9dfb5dcb623d1e7dbd5
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-06-26 02:15:18 +0200

    Modification du design

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
 	var rotation = 0;
+	var nbSlice = $(".slice").length;
+
 	$(".arrow").click(function() {
-		rotation += 60;
-		$(".menu-listings").css({
-			"transform" : 'rotate(' + rotation + 'deg)'
+		rotation++;
+		$(".slice").each(function(index) {
+			$(this).css({
+				"transform" : 'rotate(' + (rotation * 360 / nbSlice + index * 360 / nbSlice) + 'deg) skewX(30deg) scale(1)',
+				"transition" : 'transform 1s'
+			});
 		});
 	});
 
-	$('#products, #ressources, #art-school, #art-science, #forum, #old-students').hover(function() {
-		//alert("dd");
+	$(".slice").each(function(index) {
+		$(this).hover(function() {
+			$(this).css({
+				"transform" : 'rotate(' + (rotation * 360 / nbSlice + index * 360 / nbSlice) + 'deg) skewX(30deg) scale(1.15)',
+				"transition" : 'transform 1s'
+			});
+		}, function() {
+			$(this).css({
+				"transform" : 'rotate(' + (rotation * 360 / nbSlice + index * 360 / nbSlice) + 'deg) skewX(30deg) scale(1)',
+				"transition" : 'transform 1s'
+			});
+		});
 	});
 });
