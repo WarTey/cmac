c9da76607a8a51e15fd04bb17c7ac908f69817fa
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-06-27 02:40:22 +0200

    Modification du design

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,35 @@ $(document).ready(function() {
 	var rotation = 0;
 	var nbSlice = $(".slice").length;
 
+	function curveText(element) {
+		if (element.find(".text-slice").find("span").length == 0) {
+			var characters = element.find(".text-model").text().split("");
+			characters.forEach(function(item, index) {
+				element.find(".text-slice").append("<span>" + item + "</span").find("span:last").css({
+					"transform" : 'rotate(' + (2.25 * (index - characters.length / 2)) + 'deg)',
+					"display" : 'inline-block',
+					"position" : 'absolute',
+					"transform-origin" : 'bottom center',
+					"padding-bottom" : '50%'
+				});
+			});
+		}
+	}
+
+	/* 
+	** TODO : DEL
+	*/
+	var imageIndex = 1;
+	setInterval(function() {
+		$("body").css({
+			"background-image" : 'url(bg-' + imageIndex + '.jpg)'
+		});
+		if (imageIndex == 2)
+			imageIndex = 1;
+		else
+			imageIndex++;
+	}, 5000);
+
 	$(".arrow").click(function() {
 		rotation++;
 		$(".slice").each(function(index) {
@@ -18,11 +47,16 @@ $(document).ready(function() {
 				"transform" : 'rotate(' + (rotation * 360 / nbSlice + index * 360 / nbSlice) + 'deg) skewX(30deg) scale(1.15)',
 				"transition" : 'transform 1s'
 			});
+			$(this).find(".icn").fadeOut(0);
+			$(this).find(".text-slice").fadeIn();
+			curveText($(this));
 		}, function() {
 			$(this).css({
 				"transform" : 'rotate(' + (rotation * 360 / nbSlice + index * 360 / nbSlice) + 'deg) skewX(30deg) scale(1)',
 				"transition" : 'transform 1s'
 			});
+			$(this).find(".icn").fadeIn();
+			$(this).find(".text-slice").fadeOut(0);
 		});
 	});
 });
