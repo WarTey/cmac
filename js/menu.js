5fd17d219a320bd1fd6b341fd0b15192269cf884
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-07-02 23:24:17 +0200

    Correction de bug dans le responsive

--- js/menu.js
+++ js/menu.js
@@ -7,47 +7,52 @@ $(document).ready(function() {
 			$(".collapse").collapse("hide");
 	}
 
-	function resizeMenu() {
+	function menuResize() {
 		var offsetDown = $('#down').offset().top - $('#menu').offset().top;
 		var offsetLeft = $('#menu').offset().left - $('header').offset().left;
-		var diffHeight = $(window).height() - headerHeight;
 		var headerHeight = 100 * parseFloat($('header').css('height')) / parseFloat($('header').parent().css('height'));
 
 		$("header").css({
-			"height" : diffHeight < 235 ? '100%' : '100vh',
+			"height" : $(window).height() - headerHeight < 235 ? '100%' : '100vh',
 		});
+		
 		$(".navbar").css({
 			"margin-bottom" : offsetDown < 426 && offsetDown > -125 ? '-2rem' : '0',
 		});
+
 		$("#menu").css({
-			"width" : diffHeight < 235 ? '125vh' : '60vh',
+			"width" : $(window).height() - headerHeight < 235 ? '125vh' : '60vh',
 			"max-width" : function() {
 				if (parseFloat($('#menu').css('max-width')) == 520)
 					return $("header")[0].style.height == "100%" && $(window).width() < 500 ? '20em' : (offsetLeft <= 40 ? '30em' : '40em');
 				else if (parseFloat($('#menu').css('max-width')) == 390)
 					return offsetLeft <= 40 ? '25em' : (offsetLeft >= 90 ? '40em' : '30em');
 				else if (parseFloat($('#menu').css('max-width')) == 325)
-					return offsetLeft <= 40 || (diffHeight < 235 && offsetLeft <= 40) ? '20em' : (offsetLeft >= 80 ? '30em' : '25em');
+					return offsetLeft <= 40 || ($(window).height() - headerHeight < 235 && offsetLeft <= 40) ? '20em' : (offsetLeft >= 80 ? '30em' : '25em');
 				else if (parseFloat($('#menu').css('max-width')) == 260)
-					return offsetLeft <= 40 ? '15em' : ((offsetLeft >= 80 && diffHeight >= 235) || (offsetLeft >= 100 && diffHeight < 235) ? '25em' : '20em');
+					return offsetLeft <= 40 ? '15em' : ((offsetLeft >= 80 && $(window).height() - headerHeight >= 235) || (offsetLeft >= 100 && $(window).height() - headerHeight < 235) ? '25em' : '20em');
 				else if (parseFloat($('#menu').css('max-width')) == 195)
 					return offsetLeft >= 80 ? '20em' : '15em';
 			}
 		});
+
 		$("#arrow").css({
-			"margin-top" : offsetLeft < 100 || diffHeight < 235 ? '0' : 'auto',
-			"margin-left" : offsetLeft < 100 || diffHeight < 235 ? '0' : '10%',
+			"margin-top" : ($('#menu').offset().left - $('header').offset().left < 100) || ($(window).height() - headerHeight < 235) ? '0' : 'auto',
+			"margin-left" : ($('#menu').offset().left - $('header').offset().left < 100) || ($(window).height() - headerHeight < 235) ? '0' : '10%',
 			"font-size" : $("#middle").innerHeight() <= 275 ? '1.1rem' : ($("#middle").innerHeight() <= 355 ? '1.2rem' : '1.35rem'),
 		});
+
 		$("#down").css({
 			"display" : offsetDown < 426 && offsetDown > -125 ? 'none' : 'inline-block'
 		});
+
 		$("#middle").css({
 			"width" : $("#middle").innerHeight() <= 205 ? '94%' : ($("#middle").innerHeight() <= 275 ? '96%' : ($("#middle").innerHeight() <= 425 ? '98%' : ($("#middle").innerHeight() <= 500 ? '100%' : '102%'))),
 			"height": $("#middle").innerHeight() <= 205 ? '94%' : ($("#middle").innerHeight() <= 275 ? '96%' : ($("#middle").innerHeight() <= 425 ? '98%' : ($("#middle").innerHeight() <= 500 ? '100%' : '102%'))),
 			"top": $("#middle").innerHeight() <= 205 ? '3%' : ($("#middle").innerHeight() <= 275 ? '2%' : ($("#middle").innerHeight() <= 425 ? '1%' : ($("#middle").innerHeight() <= 500 ? '0' : '-1%'))),
 			"left": $("#middle").innerHeight() <= 205 ? '3%' : ($("#middle").innerHeight() <= 275 ? '2%' : ($("#middle").innerHeight() <= 425 ? '1%' : ($("#middle").innerHeight() <= 500 ? '0' : '-1%'))),
 		});
+
 		$(".slice a").css({
 			"font-size" : $("#middle").innerHeight() <= 275 ? '0.6rem' : ($("#middle").innerHeight() <= 355 ? '0.775rem' : '1rem'),
 		});
@@ -69,7 +74,7 @@ $(document).ready(function() {
 		}
 	}
 
-	resizeMenu();
+	menuResize();
 
 	$(window).scroll(function() {
 		hideCollapseNavbar();
@@ -77,7 +82,7 @@ $(document).ready(function() {
 
 	$(window).on('resize', function() {
 		hideCollapseNavbar();
-		resizeMenu();
+		menuResize();
 	});
 
 	$("#arrow").click(function() {
