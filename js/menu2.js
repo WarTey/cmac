c59b93c3caf33490974ddb119562cfdc93f1cd9a
“WarTey” <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-10-16 19:52:20 +0200

    Modification du menu

--- js/menu2.js
+++ js/menu2.js
@@ -76,22 +76,6 @@ $(document).ready(function() {
 		});
 	}
 
-	function curveText(element) {
-		if (element.find(".text-slice").find("span").length == 0) {
-			var characters = element.find(".text-model").text().split("");
-			characters.forEach(function(item, index) {
-				element.find(".text-slice").append("<span>" + item + "</span").find("span:last").css({
-					"transform" : 'rotate(' + (2.25 * (index - characters.length / 2)) + 'deg)',
-					"display" : 'inline-block',
-					"position" : 'absolute',
-					"transform-origin" : 'bottom center',
-					"padding-bottom" : '50%',
-					"max-height" : '50%',
-				});
-			});
-		}
-	}
-
 	//menuResize();
 	resizeHeader();
 	resizeMiddle();
@@ -120,22 +104,20 @@ $(document).ready(function() {
 	});
 
 	$(".slice").each(function(index) {
-		$(this).children().hover(function() {
-			$(this).parent().css({
+		$(this).hover(function() {
+			$(this).css({
 				"transform" : 'rotate(' + (rotation * 360 / nbSlice + index * 360 / nbSlice) + 'deg) skewX(30deg) scale(1.15)',
 				"transition" : 'transform 1s',
 			});
-			$(this).parent().find(".icn").fadeOut(0);
-			$(this).parent().find(".text-slice").fadeIn();
-			//curveText($(this));
-			$(this).arctext({radius: 200});
+			$(this).find(".icn").fadeOut(0);
+			$(this).find(".no-icn").fadeIn().arctext({radius: 200});
 		}, function() {
-			$(this).parent().css({
+			$(this).css({
 				"transform" : 'rotate(' + (rotation * 360 / nbSlice + index * 360 / nbSlice) + 'deg) skewX(30deg) scale(1)',
 				"transition" : 'transform 1s',
 			});
-			$(this).parent().find(".icn").fadeIn();
-			$(this).parent().find(".text-slice").fadeOut(0);
+			$(this).find(".no-icn").fadeOut(0);
+			$(this).find(".icn").fadeIn();
 		});
 	});
 });
