3470f104f5e5bfb986727620a80fa80079b36a50
“WarTey” <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-10-28 20:47:08 +0100

    Fix taille card

--- js/youtube.js
+++ js/youtube.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 		$.each(data.items, function (i, item) {
 			$('#result-iframe-youtube').append('<iframe src="https://www.youtube.com/embed/' + item.snippet.resourceId.videoId + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
 			$('#result-title-youtube').html(item.snippet.title);
-			$('#result-text-youtube').html(item.snippet.description.replace(/(\r\n|\n|\r)/gm, "<br/>").substring(0, 175) + " ...");
+			$('#result-text-youtube').html(item.snippet.description.replace(/(\r\n|\n|\r)/gm, "<br/>").substring(0, 500) + " ...");
 			$('#result-url-youtube').attr("href", "https://youtu.be/qbpaa7qzhjo" + item.snippet.resourceId.videoId);
 		})
 	});
