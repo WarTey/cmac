37061efdd76a20e4b6d87ab1aacb412be42fe4f0
“WarTey” <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-11-06 22:29:41 +0100

    Initiate DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Chapter;
+use App\Models\Content;
+use App\Models\Course;
+use App\Models\File;
+use App\Models\Level;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +19,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        Level::factory(6)->create();
+        Chapter::factory(6)->create();
+        Course::factory(6)->create();
+        Content::factory(6)->create();
+        File::factory(6)->create();
     }
 }
